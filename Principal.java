918f5302e22f0971c4207beb66b2ef1014432e1b
anab-moraes <moraesilva.ana@gmail.com>
2026-02-18 12:08:37 -0300

    Primeiro commit

---
+++ Principal.java
@@ -0,0 +1,15 @@
+package aulaJPA.aula01;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Ana Beatriz");
+		// esse código printa o texto na tela
+		
+		
+		
+	}
+
+}
